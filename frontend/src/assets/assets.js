16e7e39e705ecc8955f2c7136217ce45cc3ae9ef
G HariHaran <ghariharan200214@gmail.com>
2025-07-31 16:25:00 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -37,7 +37,7 @@ import STS from './STS.jpg';
 import Car_Parking from './Car_Parking.svg';
 import Bike_Parking from './Bike_Parking.svg';
 import truck_Parking from './truck_Parking.svg';
-import Bus_Parking from './Bus_parking.svg';
+import Bus_Parking from './Bus_Parking.svg';
 import Disable_parking from './Disable_parking.svg';
 import EV_ChargingZone from './EV_ChargingZone.svg';
 
